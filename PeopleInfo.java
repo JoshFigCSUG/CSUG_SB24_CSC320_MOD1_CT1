7663e87c6ebe2c4303ab87d564674a595d5fbee4
JoshFigCSUG <joshua.figueroa@csuglobal.edu>
2024-04-28 21:32:03 -0600

    Update PeopleInfo.java
    Fixed whitespace. Added exit message with pause.

--- PeopleInfo.java
+++ PeopleInfo.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class PeopleInfo {
 
 	public static void main(String[] args) {
-		// Declarations
+	   // Declarations
 		Scanner input = new Scanner(System.in);
 		
 		// user inputs a person's information
@@ -11,26 +11,32 @@ public class PeopleInfo {
 		String firstName = input.nextLine();
 		
 		System.out.println("Enter the person's last name: ");
-        String lastName = input.nextLine();
+      String lastName = input.nextLine();
 
-        System.out.println("Enter the person's street address: ");
-        String streetAddress = input.nextLine();
+      System.out.println("Enter the person's street address: ");
+      String streetAddress = input.nextLine();
 
-        System.out.println("Enter the person's city: ");
-        String city = input.nextLine();
+      System.out.println("Enter the person's city: ");
+      String city = input.nextLine();
 
-        System.out.println("Enter the person's zip code: ");
-        String zipCode = input.nextLine();
+      System.out.println("Enter the person's zip code: ");
+      String zipCode = input.nextLine();
 		
-     // Output person's info onto individual lines
-        System.out.println("First name: " + firstName);
-        System.out.println("Last name: "+ lastName);
-        System.out.println("Street address: " + streetAddress);
-        System.out.println("City: " + city);
-        System.out.println("Zip code: " + zipCode);
-        
-        input.close(); // close the scanner
-	// end main
-	}
+      // Output person's info onto individual lines
+      System.out.println();
+      System.out.println("First name: " + firstName);
+      System.out.println("Last name: "+ lastName);
+      System.out.println("Street address: " + streetAddress);
+      System.out.println("City: " + city);
+      System.out.println("Zip code: " + zipCode);
+      
+
+      // Good-bye message
+      System.out.println();
+      System.out.print("Press Enter to exit.");
+      input.nextLine();
+
+      input.close(); // close the scanner
+	} // end main
 
 }
